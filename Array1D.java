a39f4383e2c6fa43b6735eae4583b0bed5960b1e
Sella Putri Sari <74763171+buludhomba@users.noreply.github.com>
2021-03-23 21:55:09 +0700

    Add files via upload

---
+++ Array1D.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Root
+ */
+public class Array1D {
+
+    public static void main(String[] args) {
+
+        String[] text = {"Sella", "Putri", "Sari"};
+
+        System.out.println("Nama saya adalah :");
+
+        for (int j = 0; j < text.length; j++) {
+
+            System.out.print(text[j]);
+            System.out.print(" ");
+        }
+
+    }
+}
