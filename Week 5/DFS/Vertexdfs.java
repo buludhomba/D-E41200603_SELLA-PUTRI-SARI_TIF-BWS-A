efc50d4d3e8060f3ba025019e174d925eeaf2769
Sella Putri Sari <74763171+buludhomba@users.noreply.github.com>
2021-04-10 21:27:54 +0700

    Create Vertexdfs.java

---
+++ Week 5/DFS/Vertexdfs.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DFS;
+
+/**
+ *
+ * @root
+ */
+public class Vertexdfs {
+    public char label;
+    public boolean wasVisited;
+    
+    public Vertexdfs(char label) {
+        this.label = label; 
+        wasVisited = false;
+    }
+    
+}
